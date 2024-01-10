06bf7ab81851e52537dc0d0cb287cc67e2c8d13f
Roman <75018677+RomanJodlowski@users.noreply.github.com>
2024-01-10 09:49:36 +0000

    Delete connect_db.php

--- connect_db.php
+++
@@ -1,9 +0,0 @@
-<?php # CONNECT TO MySQL DATABASE.
-# Connect on 'localhost' for user to database 'site_db'
-$link = mysqli_connect('localhost', 'root', '', 'mktime');
-if (!$link) {
-# Otherwise fail gracefully and explain the error.
-    die('Could not connect to MySQL: ' . mysqli_error());
-}
-$error="";
-//echo 'Connection OK';
